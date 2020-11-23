305dc9546d621de962b0626968c485d7ad68f3b2
panzer1715 <74886075+panzer1715@users.noreply.github.com>
2020-11-23 10:35:44 +0700

    calculator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //import { add } from "/calculation";
 //console.log(add(2, 2));
 
-import * as cal from "/calculation";
+import * as cal from "./calculation";
 console.log(cal.add(2, 2));
 console.log(cal.multiply(2, 3));
